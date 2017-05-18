abc1b2f69ab21d6362a296a5624fc96d80eaef54
Tangoulou <tanguy.heller@gmail.com>
2017-05-18 14:15:56 +0200

    css improved
    sisi...

--- WebContent/accueil.js
+++ WebContent/accueil.js
@@ -3,18 +3,27 @@ signup = document.getElementById('signup'),
 trajet = document.getElementById('profil'),
 signin = document.getElementById('signin'),
 form_signup = document.getElementById('form_signup'),
-form_signin = document.getElementById('form_signin');
+form_signin = document.getElementById('form_signin'),
+background = document.getElementById('photofond');
+
+background.addEventListener('mouseover', highlight);
+
+function highlight(){
+	background.src = 'images/fond.jpg';
+}
 
 signin.addEventListener('click', function(e){
-	form_signin.style.display = 'block';
-	form_signup.style.display = 'none';
+	form_signin.style.height = '250px';
+	form_signin.classlist.add('open');
+	form_signup.style.height = '0px';
+//    login.focus();
 	
 	e.preventDefault();
 });
 
 signup.addEventListener('click', function(e){
-	form_signup.style.display = 'block';
-	form_signin.style.display = 'none';
+	form_signup.style.height = '500px';
+	form_signin.style.height = '0px';
 	
 	e.preventDefault();
 });
